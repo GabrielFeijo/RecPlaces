56f7ddf0c4b263265ce418472474d094ad2f7417
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2023-04-27 22:49:48 -0300

    [Fix Bugs]

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,7 @@
+import { createContext } from 'react';
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+	return <ThemeContext.Provider>{children}</ThemeContext.Provider>;
+};
