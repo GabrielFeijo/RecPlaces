3ce0da695250b592e7aea3475549d2b5d67f57b4
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2023-04-29 04:21:01 -0300

    add logout

--- src/routes/Header/Header.jsx
+++ src/routes/Header/Header.jsx
@@ -3,8 +3,14 @@ import React from 'react';
 import styles from './Header.module.css';
 import { HambergerMenu, ProfileCircle } from 'iconsax-react';
 import logo from '../../assets/logo.png';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+	const navigate = useNavigate();
+	const logout = () => {
+		localStorage.clear();
+		Navigate('/login');
+	};
 	return (
 		<div className={styles.box}>
 			<HambergerMenu
@@ -18,6 +24,7 @@ const Header = () => {
 			<ProfileCircle
 				size='32'
 				color='#000000'
+				onClick={logout}
 			/>
 		</div>
 	);
