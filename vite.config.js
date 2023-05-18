dd7a8757de4fb04bcb71449cbf3f71163b76fa5f
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2023-05-18 11:57:24 -0300

    Update VitePWA

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 	plugins: [
 		react(),
 		VitePWA({
-			includeAssets: ['src/assets/logo.png', 'src/assets/loading.png'],
+			includeAssets: ['/assets/logo.png', '/loading.png', , 'loading.png'],
 			registerType: 'autoUpdate',
 			manifest: {
 				background_color: '#1aa7ec',
