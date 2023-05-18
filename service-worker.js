0b6b7d3c5141fd4bed7939ce767ed76a046e23e3
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2023-05-18 09:02:43 -0300

    [NEW] PWA

--- service-worker.js
+++ service-worker.js
@@ -4,10 +4,11 @@ importScripts(
 	'https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js'
 );
 
-const CACHE = 'pwabuilder-page';
+const CACHE = 'RecPlaces-page';
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = [
+	'index.html',
 	'./src/routes/Home/Home.jsx',
 	'./src/routes/Home/Home.module.css',
 	'./src/routes/Home/Menu.jsx',
